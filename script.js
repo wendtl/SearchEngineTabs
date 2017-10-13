58a28680f35e700e0e8ed9d9bd7df53220d9c4f0
Logan <lwendt220@gmail.com>
2017-10-13 16:35:35 -0500

    Small changes
    figuring out how to grab URL from active tab.

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
+function switchToGoogle() {
+    console.log(getSearchInfo());
+}
+
+function switchToDDG() {
+
+}
+
+function switchToBing() {
+
+}
+
+// Extract search type and search terms
+function getSearchInfo() {
+    chrome.tabs.getCurrent(function(tab){
+        console.log(tab.url);
+    });
+}
+
 var body = document.getElementsByTagName('body');
-var header = document.createElement('header');
-header.innerHTML = "<header>" +
-    "<ul id=\"searchTabs\">" +
-        "<li>Google</li>" +
-        "<li>DuckDuckGo</li>" +
-        "<li>Bing</li>" +
-    "</ul>" +
+var headerDiv = document.createElement('div');
+headerDiv.innerHTML = "<header>\n" +
+    "<ul id=\"searchTabs\">\n" +
+        "<li><button id=\"googleBtn\">Google</button>" +
+        "<li><button id=\"ddgBtn\">DuckDuckGo</button></li>\n" +
+        "<li><button id=\"bingBtn\">Bing</button></li>\n" +
+    "</ul>\n" +
 "</header>";
-document.body.innerHTML = header.innerHTML + document.body.innerHTML;
+document.body.innerHTML = headerDiv.innerHTML + document.body.innerHTML;
+
+document.addEventListener('DOMContentLoaded', function () {
+    document.getElementById("googleBtn").addEventListener("click", switchToGoogle);
+    document.getElementById("ddgBtn").addEventListener("click", switchToGoogle);
+    document.getElementById("bingBtn").addEventListener("click", switchToGoogle);
+    document.querySelector('button').addEventListener('click', clickHandler);
+    main();
+  });
